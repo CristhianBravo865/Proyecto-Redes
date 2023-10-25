87755966219e958afeeb1f7595d9ee31bec8d8de
Cristhian Matias Bravo Arredondo <cristhian.bravo8657@gmail.com>
2023-10-25 17:51:56 -0500

    Main v0.4 - Interfaz Grafica Funcional

--- src/Exec/Main.java
+++ src/Exec/Main.java
@@ -1,71 +1,86 @@
 package Exec;
 import Bases.*;
 import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        while(true){
-            System.out.println("Ingrese la base del número inicial: Base 10(10) Base 2(2) Base 8(8) Base Hexadecimal(1F)");
-            String cb1 = sc.nextLine();
-            Base base1 = null; 
+        JFrame frame = new JFrame("Conversor de Bases");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 200);
+        frame.setLayout(new BorderLayout());
 
-            try {
-                if (cb1.equals("10")) {
-                    System.out.println("Ingrese el número en base 10: ");
-                    String b = sc.nextLine();
-                    base1 = new Decimal(b);
-                } 
-                else if (cb1.equals("2")) {
-                    System.out.println("Ingrese el número en base 2: ");
-                    String b = sc.nextLine();
-                    base1 = new Binario(b);
-                } 
-                else if (cb1.equals("8")) {
-                    System.out.println("Ingrese el número en base 8: ");
-                    String b = sc.nextLine();
-                    base1 = new Octal(b);
-                } 
-                else if (cb1.equals("1F")) {
-                    System.out.println("Ingrese el número en base hexadecimal: ");
-                    String b = sc.nextLine();
-                    base1 = new Hexadecimal(b);
-                } 
-                else {
-                    System.out.println("Base no válida. Debe ser 10, 2, 8 o 1F.");
-                    continue;
-                }
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-                continue;
-            }
-            while(true){
-                    System.out.println("A qué base-Base 10(10) Base 2(2) Base 8(8) Base Hexadecimal(1F)- desea transformar " + base1);
-                    String cb2 = sc.nextLine();
-                    try {
-                        if (cb2.equals("10")) {
-                            System.out.println(base1.convertirDecimal());
-                        } else if (cb2.equals("2")) {
-                            System.out.println(base1.convertirBinario());
-                        } else if (cb2.equals("8")) {
-                            System.out.println(base1.convertirOctal());
-                        } else if (cb2.equals("1F")) {
-                            System.out.println(base1.convertirHexadecimal());
-                        } else {
-                            System.out.println("Base de salida no válida. Debe ser 10, 2, 8 o 1F.");
-                            continue;
-                        }
-                        break;
-                    } catch (IllegalArgumentException e) {
-                        System.out.println(e.getMessage());
-                        continue;
+        JPanel inputPanel = new JPanel(new GridLayout(3, 2));
+        JPanel outputPanel = new JPanel(new BorderLayout());
+        JLabel labelBaseNum = new JLabel("Base del número inicial (10, 2, 8, 16):");
+        JTextField BaseNumField = new JTextField(5);
+        JLabel labelNumero = new JLabel("Número:");
+        JTextField numeroField = new JTextField(10);
+        JButton botonConvertir = new JButton("Convertir");
+        JTextArea resultados = new JTextArea(10, 20);
+        resultados.setEditable(false);
+
+        inputPanel.add(labelBaseNum);
+        inputPanel.add(BaseNumField);
+        inputPanel.add(labelNumero);
+        inputPanel.add(numeroField);
+        inputPanel.add(new JLabel());
+        inputPanel.add(botonConvertir);
+        outputPanel.add(new JScrollPane(resultados), BorderLayout.CENTER);
+
+        frame.add(inputPanel, BorderLayout.NORTH);
+        frame.add(outputPanel, BorderLayout.CENTER);
+        frame.setVisible(true);
+        botonConvertir.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evento) {
+                String  cbase1 = BaseNumField.getText();
+                String  numero = numeroField.getText();
+                Base base1 = null; 
+                try {
+                    if (cbase1.equals("10")) {
+                        base1 = new Decimal(numero);
+                    } 
+                    else if (cbase1.equals("2")) {
+                        base1 = new Binario(numero);
+                    } 
+                    else if (cbase1.equals("8")) {
+                        base1 = new Octal(numero);
+                    } 
+                    else if (cbase1.equals("16")) {
+                        base1 = new Hexadecimal(numero);
+                    } 
+                    else {
+                        resultados.setText("Base no válida. Debe ser 10, 2, 8 o 16.");
+                        return;
                     }
-            }
-            System.out.println("¿Quiere seguir usando el porgrama? (S/N)");
-            String controlbuclepri=sc.nextLine();
-            if (!controlbuclepri.equalsIgnoreCase("S")){
-                break;
-            }
+                } catch (IllegalArgumentException e) {
+                    resultados.setText(e.getMessage());
+                    return;
+                }
+
+                String cbase2 = JOptionPane.showInputDialog("A qué base desea transformar " + base1 + " (10, 2, 8, 16)?");
+
+                        try {
+                            if (cbase2.equals("10")) {
+                                resultados.setText(String.valueOf(base1.convertirDecimal()));
+                            } else if (cbase2.equals("2")) {
+                                resultados.setText(base1.convertirBinario());
+                            } else if (cbase2.equals("8")) {
+                                resultados.setText(base1.convertirOctal());
+                            } else if (cbase2.equals("16")) {
+                                resultados.setText(base1.convertirHexadecimal());
+                            } else {
+                                resultados.setText("Base de salida no válida. Debe ser 10, 2, 8 o 16.");
+                            }
+                        } catch (IllegalArgumentException e) {
+                            resultados.setText(e.getMessage());
+                        }
+                    }
+                });
         }
-    }
 }
+
